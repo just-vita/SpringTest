2aeba80aef4a06b72b44b2ecce3ee56119151a17
login <1254581982@qq.com>
2022-02-24 22:02:24 +0800

    1.5.1

--- SpringTest2/src/test/java/Test1.java
+++ SpringTest2/src/test/java/Test1.java
@@ -1,16 +1,24 @@
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 
 public class Test1 {
     @Test
     public void test1() throws Exception{
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql:///travel?serverTimezone=UTC&Unicode=true&characterEncoding=UTF-8");
-        dataSource.setUser("root");
-        dataSource.setPassword("zxc758203");
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+//        dataSource.setJdbcUrl("jdbc:mysql:///travel?serverTimezone=UTC&Unicode=true&characterEncoding=UTF-8");
+//        dataSource.setUser("root");
+//        dataSource.setPassword("zxc758203");
+//        Connection connection = dataSource.getConnection();
+//        System.out.println(connection);
+//        connection.close();
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DataSource dataSource = (DataSource)app.getBean("dataSource");
         Connection connection = dataSource.getConnection();
         System.out.println(connection);
         connection.close();
