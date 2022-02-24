acc6bc759739f93ec213d97e8a7a6d7e4c0771b8
login <1254581982@qq.com>
2022-02-24 14:13:02 +0800

    3.5

--- src/main/java/com/test/demo.java
+++ src/main/java/com/test/demo.java
@@ -1,10 +1,14 @@
 package com.test;
 
 import com.test.impl.UserServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class demo {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
+//        UserService userService = new UserServiceImpl();
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        UserService userService = (UserService) app.getBean("userService");
         userService.save();
     }
 }
