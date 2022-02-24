3922450c042b76d9144c4b0f8bc3a77c9ec59fcd
login <1254581982@qq.com>
2022-02-24 15:38:09 +0800

    1.2

--- src/test/java/UserDemo.java
+++ src/test/java/UserDemo.java
@@ -1,13 +1,22 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import com.test.UserDao;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+
 public class UserDemo {
     @Test
-    public void test1(){
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
-        UserDao dao = (UserDao) app.getBean("userDao");
-        dao.save();
+    public void test1() throws Exception {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql:///travel?serverTimezone=UTC&Unicode=true&characterEncoding=UTF-8");
+        dataSource.setUser("root");
+        dataSource.setPassword("zxc758203");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
     }
 }
