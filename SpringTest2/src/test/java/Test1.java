0856975d6d6370e4cabdd61d155589f6dc28c947
login <1254581982@qq.com>
2022-02-24 16:17:51 +0800

    1.3.1

---
+++ SpringTest2/src/test/java/Test1.java
@@ -0,0 +1,18 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class Test1 {
+    @Test
+    public void test1() throws Exception{
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql:///travel?serverTimezone=UTC&Unicode=true&characterEncoding=UTF-8");
+        dataSource.setUser("root");
+        dataSource.setPassword("zxc758203");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+}
