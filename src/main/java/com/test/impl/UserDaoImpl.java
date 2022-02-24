2d3a11281646a260b87542f1420d4ed8f3f7287f
login <1254581982@qq.com>
2022-02-24 14:44:39 +0800

    3.8.1

--- src/main/java/com/test/impl/UserDaoImpl.java
+++ src/main/java/com/test/impl/UserDaoImpl.java
@@ -3,9 +3,29 @@ package com.test.impl;
 import com.test.UserDao;
 
 public class UserDaoImpl implements UserDao {
+    private String name;
+    private int age;
+
+    public UserDaoImpl() {
+    }
+
+    public UserDaoImpl(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     @Override
     public void save() {
+        System.out.println(name+"==="+age);
         System.out.println("method save invoded...");
+
     }
 }
