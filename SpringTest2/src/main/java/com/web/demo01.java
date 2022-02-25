b490b8d31c5d6d4e71d9dc2ba90cec832883694e
login <1254581982@qq.com>
2022-02-25 17:47:54 +0800

    2.1.2 : added some Spring annotation

--- SpringTest2/src/main/java/com/web/demo01.java
+++ SpringTest2/src/main/java/com/web/demo01.java
@@ -1,12 +1,17 @@
 package com.web;
 
+import com.config.SpringConfiguration;
 import com.service.UserService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 public class demo01 {
     public static void main(String[] args) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext app = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         UserService userService = app.getBean(UserService.class);
         userService.save();
     }
