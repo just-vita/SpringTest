300241e309d5d8aab749a361213fa5915dfe7366
login <1254581982@qq.com>
2022-02-25 23:02:03 +0800

    optimized some code (useful)

--- SpringMvcTest/src/main/java/com/web/listener/ContextLoaderListener.java
+++ SpringMvcTest/src/main/java/com/web/listener/ContextLoaderListener.java
@@ -2,6 +2,8 @@ package com.web.listener;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
