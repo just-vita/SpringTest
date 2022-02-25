d464f0db8d75ffc94596db6d570614af566a8774
login <1254581982@qq.com>
2022-02-25 22:51:53 +0800

    optimized some code (useful)

--- SpringMvcTest/src/main/java/com/web/listener/ContextLoaderListener.java
+++ SpringMvcTest/src/main/java/com/web/listener/ContextLoaderListener.java
@@ -2,6 +2,8 @@ package com.web.listener;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -10,8 +12,8 @@ import javax.servlet.ServletContextListener;
 public class ContextLoaderListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         ServletContext servletContext = servletContextEvent.getServletContext();
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
         servletContext.setAttribute("app",app);
         System.out.println("spring container created--------------------------");
     }
--- SpringMvcTest/src/main/java/com/web/servlet/UserServlet.java
+++ SpringMvcTest/src/main/java/com/web/servlet/UserServlet.java
@@ -3,6 +3,7 @@ package com.web.servlet;
 import com.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -17,7 +18,8 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext servletContext = req.getServletContext();
-        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
+//        ApplicationContext app = (ApplicationContext) servletContext.getAttribute("app");
+        ApplicationContext app = WebApplicationContextUtils.getWebApplicationContext(servletContext);
         UserService userService = app.getBean(UserService.class);
         userService.save();
     }
