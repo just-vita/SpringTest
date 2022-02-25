a55b2601b106abf0bd0a865f493a29758ee1e50f
login <1254581982@qq.com>
2022-02-25 17:21:38 +0800

    2.1.2 add anno scope and value

--- SpringTest2/src/main/java/com/service/impl/UserServiceImpl.java
+++ SpringTest2/src/main/java/com/service/impl/UserServiceImpl.java
@@ -4,10 +4,19 @@ import com.dao.UserDao;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Service("userService")
+@Scope("singleton")
 public class UserServiceImpl implements UserService {
+    @Value("${jdbc.driver}")
+    private String str;
+
     @Autowired
     @Qualifier("userDao")
     private UserDao userDao;
@@ -18,5 +27,15 @@ public class UserServiceImpl implements UserService {
 
     public void save() {
         userDao.save();
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("init......");
+    }
+
+    @PreDestroy
+    public void destory(){
+        System.out.println("destory......");
     }
 }
