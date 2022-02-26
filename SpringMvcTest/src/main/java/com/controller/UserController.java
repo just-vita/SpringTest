552bf8db652aa73f152cf2800f778d3447ef2804
login <1254581982@qq.com>
2022-02-26 15:59:12 +0800

    added some SpringMVC optimized

--- SpringMvcTest/src/main/java/com/controller/UserController.java
+++ SpringMvcTest/src/main/java/com/controller/UserController.java
@@ -2,13 +2,24 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/test")
     public String save(){
         System.out.println("Controller save ......");
-        return "test.jsp";
+        return "test";
+    }
+
+    @RequestMapping("/test2")
+    public ModelAndView save2(){
+        System.out.println("Controller save ......");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("test");
+        modelAndView.addObject("username","abcdefg");
+        return modelAndView;
     }
 }
--- SpringMvcTest/src/main/java/com/service/impl/UserServiceImpl.java
+++ SpringMvcTest/src/main/java/com/service/impl/UserServiceImpl.java
@@ -2,14 +2,6 @@ package com.service.impl;
 
 import com.dao.UserDao;
 import com.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 public class UserServiceImpl implements UserService {
 
