3298ab479e6963330e6c8f24ebf1e4cd4c6a3e2c
login <1254581982@qq.com>
2022-02-27 23:37:49 +0800

    initiate project

---
+++ SpringJdbc/src/test/java/com/JdbcTemplateCRUDTest.java
@@ -0,0 +1,21 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class JdbcTemplateCRUDTest {
+    @Autowired
+    private JdbcTemplate template;
+
+    @Test
+    public void test1(){
+        int update = template.update("update user set password = 5555 where id = ?", 4);
+        System.out.println(update);
+    }
+}
--- SpringJdbc/src/test/java/com/test.java
+++ SpringJdbc/src/test/java/com/test.java
@@ -1,4 +1,29 @@
 package com;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 public class test {
+    @Test
+    public void test1() throws SQLException {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        DataSource dataSource = (DataSource) app.getBean("dataSource");
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        int update = jdbcTemplate.update("insert into user values(?,?,?)",3,"123","123");
+        System.out.println(update);
+    }
+
+    @Test
+    public void test2() throws SQLException {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+        JdbcTemplate jdbcTemplate = (JdbcTemplate) app.getBean("jdbcTemplate");
+        int update = jdbcTemplate.update("insert into user values(?,?,?)",4,"1234","1234");
+        System.out.println(update);
+    }
 }
